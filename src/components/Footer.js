e92cb756b5f086190f08597981af4eb691fccaa4
houda merbah <houdamerbah111@gmail.com>
2023-05-20 02:35:55 +0200

    1st commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container >
+  
+    </Container>
+  );
+}
+
+export default Footer;
